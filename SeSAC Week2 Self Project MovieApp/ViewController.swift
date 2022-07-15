78965a2e5cfdac86b690c9fc7133e285b853ea38
roargenie <105975078+roargenie@users.noreply.github.com>
2022-07-15 21:42:06 +0900

    Initial Commit

---
+++ SeSAC Week2 Self Project MovieApp/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  SeSAC Week2 Self Project MovieApp
+//
+//  Created by 이명진 on 2022/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.hidesBarsOnSwipe = true
+        
+        
+        
+        
+        
+    }
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
